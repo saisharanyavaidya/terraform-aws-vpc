25536ed3c0fc0471bbeef3af909d7c7772b59eb6
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-05-22 00:08:16 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,134 @@
+#### Project variables ####
+
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "common_tags" {
+    type = map
+}
+
+
+#### VPC related variables ####
+
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
+
+#### igw ####
+
+variable "igw_tags" {
+    type = map
+    default = {}
+}
+
+#### public subnet ####
+
+variable "public_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.public_subnet_cidrs) == 2
+      error_message = "please provide 2 valid public subnet cidr"
+    }
+}
+
+variable "public_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+#### private subnet ####
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.private_subnet_cidrs) == 2
+      error_message = "please provide 2 valid private subnet cidr"
+    }
+}
+
+variable "private_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+#### database subnet ####
+
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+      condition = length(var.database_subnet_cidrs) == 2
+      error_message = "please provide 2 valid database subnet cidr"
+    }
+}
+
+variable "database_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_group_tags" {
+    type = map
+    default = {}
+}
+
+#### NAT Gateway tags ####
+
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+}
+
+#### Public Route table ####
+
+variable "public_route_table_tags" {
+  type = map
+  default = {}
+}
+
+#### Private Route table ####
+
+variable "private_route_table_tags" {
+  type = map
+  default = {}
+}
+
+#### Database Route table ####
+
+variable "database_route_table_tags" {
+  type = map
+  default = {}
+}
+
+#### Peering ####
+
+variable "is_peering_required" {
+  type = bool
+  default = false
+}
+
+variable "acceptor_vpc_id" {
+  type = string
+  default = ""
+}
+
+variable "vpc_peering_tags" {
+    type = map
+    default = {}
+}
